759b9550a7c82f3b4ee0210b601ce8385b9e05f3
v <mondalbdyut@gmail.com>
2023-04-15 08:00:48 +0530

    sixth commit

--- index.js
+++ index.js
@@ -47,8 +47,8 @@ async function Insert() {
     try {
         const NewData = new ClassModel({
             name: "bidyut kumar mandal4",
-            phone: "4234563895",
-            email: "bidyut44.mandal@gmail.com",
+            phone: 1234563892,
+            email: "bidyut41.mandal@gmail.com",
             age: 18,
             active: true,
         });
@@ -101,33 +101,33 @@ async function ReadData() {
 
 async function DataUpdate(id) {
     try {
-        if (id.length !== 24) {
+        if (id.length < 24) {
             return console.log("failed..!! Invalid Id. id must be 24 characters.");
         }
         let total = await ClassModel.find({ _id: new mongodb.ObjectId(id) }).countDocuments();
-        if (total == 0) {
+        if (total <= 0) {
             return console.log("record not found..!!");
         }
-        // let result = await ClassModel.findByIdAndUpdate(
-        //     { _id: new mongodb.ObjectId(id) },
-        //     {
-        //         $set: { name: "hello MongoDB", updated_at: Date.now() }
-        //     },
-        //     {
-        //         new: true,
-        //         useFindAndModify: false
-        //     });
+        let result = await ClassModel.findByIdAndUpdate(
+            { _id: new mongodb.ObjectId(id) },
+            {
+                $set: { name: "hello MongoDB", updated_at: Date.now() }
+            },
+            {
+                new: true,
+                useFindAndModify: false
+            });
         // let result = await ClassModel.updateOne({ age: { $gte: 20 } }, { $set: { updated_at: Date.now() } });
-        let result = await ClassModel.updateMany({ active: { $eq: false } }, {
-            $set: {
-                updated_at: Date.now(),
-                social_acc: [
-                    { "facebook": 1 },
-                    { "twitter": 0 },
-                    { "instagram": 0 }
-                ]
-            }
-        });
+        // let result = await ClassModel.updateMany({ active: { $eq: false } }, {
+        //     $set: {
+        //         updated_at: Date.now(),
+        //         social_acc: [
+        //             { "facebook": 1 },
+        //             { "twitter": 0 },
+        //             { "instagram": 0 }
+        //         ]
+        //     }
+        // });
         console.log(result);
     } catch (error) {
         console.log(error);
@@ -172,7 +172,7 @@ async function DeleteUpdate(id) {
 //Comparison Query Operators
 async function Comparison() {
     try {
-        // let data = await ClassModel.find({ age: { $eq: 20 } });
+        //let data = await ClassModel.find({ age: { $eq: 20 } });
         //let data = await ClassModel.find({ age: { $gt: 20 } });
         //let data = await ClassModel.find({ age: { $gte: 20 } });
         //let data = await ClassModel.find({ age: { $lt: 20 } });
@@ -188,12 +188,13 @@ async function Comparison() {
 //Comparison();
 
 //Logical Query Operators
+//$expr -> expression operator
 async function Logical() {
     try {
         //let data = await ClassModel.find({ $and: [{ email: "vidyut.start006@gmail.com" }, { name: "vidyut1" }] });
         //let data = await ClassModel.find({ age: { $not: { $gt: 20 } } });
-        let data = await ClassModel.find({ $nor: [{ age: 19 }, { age: 30 }] });
-        // let data = await ClassModel.find({ $or: [{ age: 20 }, { name: "vidyut1" }] });
+        // let data = await ClassModel.find({ $nor: [{ age: 19 }, { age: 30 }] });
+        let data = await ClassModel.find({ $or: [{ age: 20 }, { name: "vidyut1" }] });
         console.log(data);
     } catch (error) {
         console.log(error);
@@ -209,7 +210,7 @@ async function DataCountAndSorting() {
 
         //let data = await ClassModel.find({ age: { $gte: 20 } }).countDocuments();
 
-        //let data = await ClassModel.find().select({ name: 1, age: 1 }).sort({ age: 1 });
+        //let data = await ClassModel.find().select({ name: 1, age: 1 }).sort({ age: -1 });
 
         //let date = new Date('2022-11-19T15:55:55.160+00:00');
         //let timeis = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}:${date.getMilliseconds()}`;
@@ -221,8 +222,9 @@ async function DataCountAndSorting() {
 
 
         //LIKE OPERATER IN MONGODB
-        // let data = await ClassModel.find({ name: { $regex: /mongoDB/i } });
-        //let data = await ClassModel.find({ name: { $in: [/mongodb/i, /mandal4/i] } });
+        //let data = await ClassModel.aggregate([{ $match: { 'name': { $regex: new RegExp(name), $options: "i" } } }, { $project: { _id: 1, name: 1 } }]);
+        //let data = await ClassModel.find({ name: { $regex: new RegExp('mongoDB'), $options: "i" } });//{ $regex: /mongoDB/i }
+        let data = await ClassModel.find({ name: { $in: [/mongodb/i, /mandal4/i] } });
 
         //let data = await ClassModel.find().limit(2);
 
@@ -232,7 +234,7 @@ async function DataCountAndSorting() {
         // let data = await ClassModel.find({ 'social_acc': { $elemMatch: { facebook: 0 } } });
 
         //Distinct
-        let data = await ClassModel.distinct('age', { active: true });
+        //let data = await ClassModel.distinct('age', { active: true });
         console.log(data);
         // var allowlist = ['http://localhost:4200', 'http://example2.com'];
         // if (allowlist.indexOf("http://localhost:4200") == -1) {
